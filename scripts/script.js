ed47c3258881b91da45c038cdf4c5ed9975c6ba1
Julia Xu <jlxu@ucsd.edu>
2021-06-06 13:46:49 -0400

    add split code

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,8 @@
 
 import { router } from './router.js';
 
+
+
 const headerText = document.querySelector('header > h1');
 const settings = document.querySelector('header > img');
 
@@ -38,4 +40,61 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelector('main').appendChild(newPost);
       });
     });
+});
+
+
+
+
+
+// Instantiate the SDK. CDN will expose splitio globally 
+var factory = splitio({ 
+  core: {
+    authorizationKey: 'YOUR_API_KEY',
+    // your internal user id, or the account id that 
+    // the user belongs to. 
+    // This coudld also be a cookie you generate
+    // for anonymous users
+    key: 'key',
+    // an OPTIONAL traffic type, if provided will be
+    // used for event tracking with the SDK client.
+    trafficType: 'A_TRAFFIC_TYPE'
+  }
+});
+// And get the client instance you'll use
+var client = factory.client();
+
+client.on(client.Event.SDK_READY, function() {
+  var attributes = {
+    // date attributes are handled as `millis since epoch`
+    registered_date: new Date('YYYY-MM-DDTHH:mm:ss.sssZ').getTime(),
+    // this string will be compared against a list called `plan_type`
+    plan_type: 'growth',
+    // this number will be compared agains a const value called `deal_size`
+    deal_size: 10000,
+    // this boolean will be compared against a const value called `paying_customer`
+    paying_customer: true,
+    // this array will be compared against a set called `permissions`
+    permissions: ["read", "write"]
+  };
+  
+  var treatment = client.getTreatment('SPLIT_NAME', attributes);
+  if (treatment == "on") {
+      // insert code here to show on treatment
+  } else if (treatment == "off") {
+      // insert code here to show off treatment
+  } else {
+      // insert your control treatment code here
+  }
+});
+
+
+// You can just destroy and remove the variable reference and move on:
+user_client.destroy();
+user_client = null;
+// destroy() returns a promise, so if you want to, for example,
+// navigate to another page without loosing impressions, you 
+// can do that once the promise resolves.
+user_client.destroy().then(function() {
+     user_client = null;
+  document.location.replace('another_page');
 });
